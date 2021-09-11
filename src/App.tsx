ee60190ca0a560bf3d8bd143177ae1f1cdad5a84
“rosel.jeni” <rosel.jeni@gmail.com>
2021-09-12 01:21:40 +0900

    add shop page, collection preview component, and collecton item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { Link as RouterLink } from "react-router-dom";
-
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => {
-  return <div>
-    <h1>Hats Page</h1>
-  </div>
-}
-
+import './App.scss'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
-
-      {/* <HomePage /> */}
     </div>
   );
 }
